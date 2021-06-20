57c8674beb279fb27a7e58cefd834d75999cd080
Anisote <anisote@protonmail.com>
2021-06-20 08:40:41 +0200

    Fix typo

--- faq.php
+++ faq.php
@@ -9,19 +9,19 @@
     <p>
       <b></br>Pourquoi est-ce qu’une source apparaît ici ?</b></br>
 
-      Pour figuré ici, une source doit répondre à ces différents critères :
+      Pour figurer ici, une source doit répondre à ces différents critères :
       <ul>
-          <li>Qualité de l’information (sourcée, semblait véridique)</li>
+          <li>Qualité de l’information (sourcée, semble factuelle)</li>
           <li>Cohérence du raisonnement</li>
           <li>Honnêté intellectuelle de l’auteur</li>
-          <li>Capacité de l’auteur à remettre en question ses opionions, raisonnements et à changer d’avis</li>
+          <li>Capacité de l’auteur à remettre en question ses opinions, raisonnements et à changer d’avis</li>
           <li>Sensibilité à la critique</li>
           <li>Être sélectionnée par l’auteur du site</li>
       </ul>
 
       <b>Quand est-ce que le site est mis-à-jour ?</b></br>
 
-      Le site est mis-à-jour au fil de l’eau.
+      Le site est mis à jour au fil de l’eau.
 
       </br><b></br>Un contenu que j'estime très bon n’est pas présent, est-ce normal ?</b></br>
 
@@ -33,16 +33,16 @@
     <p>
       <b>Qui suis-je ?</b></br>
 
-      Je suis un informaticien d’une trentaine d’année et je vis en France et je publie sous pseudonymat.
+      Je suis une personne d’une trentaine d’année, je vis en France et je publie sous pseudonymat.
 
 
       </br><b></br>Pourquoi est-ce que je fais ce site ?</b></br>
 
-      Je fais ce site afin de partager des sources que j’estime de qualité et qui peuvent être difficiles à trouver. Il a également pour fonction de garder une trace afin de permettre une relecture/visionnage ultérieur.
+      Je fais ce site afin de partager des sources que j’estime de qualité et qui peuvent être difficiles à trouver. Il a également pour fonction de garder une trace afin de permettre un visionnage ou une relecture ultérieur.
 
       </br><b></br>Pourquoi avoir choisir le pseudonyme Anisote ?</b></br>
 
-      C’est une référence à l’Anisote de l’Erable et j’aimais bien le fait qu’il y ait la sonorité “sote” dedans.
+      C’est une référence à l’Anisote de l’Érable et j’aimais bien le fait qu’il y ait la sonorité “sote” dedans.
     </p>
   </div>
 
