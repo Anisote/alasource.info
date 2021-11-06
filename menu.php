9cf38c467bf13aa866a2fdadbe9d85acc4ba0ec1
Anisote <anisote@protonmail.com>
2021-11-06 20:52:42 +0100

    Improve h2 size on mobile phone

--- menu.php
+++ menu.php
@@ -45,7 +45,7 @@
         <div class="navbar-nav me-auto"></div>
         <ul class="navbar-nav ml-auto">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="index.php">Accueil</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="faq.php">Foire aux questions</a>
