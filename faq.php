3f146c647eb9997bb932318a06136e94381b1f92
Anisote <anisote@protonmail.com>
2021-11-06 20:18:55 +0100

    Rewrite the faq

--- faq.php
+++ faq.php
@@ -2,12 +2,11 @@
   require_once('menu.php');
 ?>
 
-
 <div id="content">
 <p class ="center-15">
     <blockquote>
       Parce qu'il est vital d'avoir des informations d'excellente qualité afin de mieux comprendre le monde qui nous entoure et de prendre les bonnes décisions. 
-      J'ai créé ce site afin de vous partager le nom des contenus qui m'ont enrichi intellectuellement et m'ont fait voir les choses différemment. 
+      J'ai créé ce site afin de vous partager les noms des contenus qui m'ont enrichi intellectuellement et m'ont fait voir les choses différemment. 
       J'espère qu'ils vous seront aussi profitables qu'à moi.<i>Anisote</i>
     </blockquote>
   </p>
@@ -16,23 +15,30 @@
   <p>
     <b>Pourquoi est-ce qu’une source apparaît ici ?</b></br>
 
-    Pour figurer ici, une source doit répondre à ces différents critères :
+    Pour figurer ici, une source réponds à un ou plusieurs des critères ci-dessous : 
     <ul>
-        <li>Qualité de l’information (sourcée, semble factuelle)</li>
-        <li>Cohérence du raisonnement</li>
-        <li>Honnêté intellectuelle de l’auteur</li>
-        <li>Capacité de l’auteur à remettre en question ses opinions, raisonnements et à changer d’avis</li>
-        <li>Sensibilité à la critique</li>
-        <li>Être sélectionnée par l’auteur du site</li>
+        <li>Qualité de l’information (source, cohérence du raisonnement)</li>
+        <li>Rareté de l’information dans les informations grand publique</li>
+        <li>Expertise de l'auteur reconnue par ses pairs</li>
     </ul>
 
     <b>Quand est-ce que le site est mis-à-jour ?</b></br>
 
-    Le site est mis à jour au fil de l’eau.
+    Le site est mis à jour au fil de l'eau
 
     </br><b></br>Un contenu que j'estime très bon n’est pas présent, est-ce normal ?</b></br>
 
-    Je ne suis malheureusement qu'un homme avec des ressources limitées.
+    Oui, je n'ai malheureusement que peu de temps à accorder à ce site. N'hésitez pas à me transmettre des liens vers des sources de bonnes qualités. Si je les trouve également de bonnes qualités, elles figureront peut-être ici.
+
+    </br><b></br>Un contenu ou un auteur avec lequel je suis en désaccord est présent ici. Est-ce normal ?</b></br>
+    
+    Oui, c'est également le cas pour moi mais être en désaccord ne veut pas dire que ce n'est pas intéressant. De plus, cela fait évoluer la compréhension du sujet.
+
+    </br><b></br>Un contenu de mauvaise qualité est présent ici. Que puis-je faire pour le faire supprimer ?</b></br>
+
+    Vous pouvez m'envoyer un contenu de meilleure qualité. Si je partage votre avis, je n'hésiterai pas à mettre à jour la liste, ce qui permettra à tous d'améliorer sa compréhesion du sujet.
+
+
   </p>
 
   <h2>L'auteur</h2>
@@ -42,10 +48,17 @@
 
     Je suis une personne d’une trentaine d’année, je vis en France et je publie sous pseudonymat.
 
-
     </br><b></br>Pourquoi est-ce que je fais ce site ?</b></br>
 
-    Je fais ce site afin de partager des sources que j’estime de qualité et qui peuvent être difficiles à trouver. Il a également pour fonction de garder une trace afin de permettre un visionnage ou une relecture ultérieur.
+    Je fais ce site pour plusieurs raison :     
+    
+    <ul>
+        <li>Avoir une liste de sources que j'estime de qualité facilement consultable</li>
+        <li>Pour partager facilement les sources que j'estime de qualité</li>
+        <li>Développer un outil de bibliothèque de liens open source que d'autres puissent s'accaparer afin de publier les contenus qu'ils trouvent intéressants</li>
+        <li>Permettre l'émergence d'un référentiel de savoirs d'excellentes qualités non parasitées</li>
+        <li>Permettre à tous de mieux comprendre certains sujets complexes en ayant en leur disposition des sources d'excellentes qualités</li>
+    </ul>
 
     </br><b></br>Pourquoi avoir choisir le pseudonyme Anisote ?</b></br>
 
