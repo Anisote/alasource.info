dca3b3b25eaa44dd3bb3b743c6dee2fc780e5971
Anisote <anisote@protonmail.com>
2021-11-14 11:22:08 +0100

    Replace sitemap.xml by sitemap.php and generate it from the server side - 2

---
+++ sitemap.php
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+            http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+
+<url>
+  <loc>https://alasource.info/</loc>
+  <lastmod><?= date("c", filemtime("index.php")) ?></lastmod>
+  <priority>1.00</priority>
+</url>
+<url>
+  <loc>https://alasource.info/faq.php</loc>
+  <lastmod><?= date("c", filemtime("faq.php")) ?></lastmod>
+  <priority>0.80</priority>
+</url>
+<url>
+  <loc>https://alasource.info/contact.php</loc>
+  <lastmod><?= date("c", filemtime("contact.php")) ?></lastmod>
+  <priority>0.50</priority>
+</url>
+<url>
+  <loc>https://alasource.info/mentionslegales.php</loc>
+  <lastmod><?= date("c", filemtime("mentionslegales.php")) ?></lastmod>
+  <priority>0.50</priority>
+</url>
+
+
+</urlset>
